548c31a6bf57778b1681990cb20e1a761b8a89a0
milaq <micha.laqua@gmail.com>
2022-10-08 12:34:54 +0200

    add launcher stub
    to allow some apps to index the launcher (e.g. Kindle Fire)

---
+++ app/src/main/java/com/milaq/settingslauncher/Launcher.java
@@ -0,0 +1,7 @@
+package com.milaq.settingslauncher;
+
+import android.app.Application;
+
+public class Launcher extends Application {
+    // STUB - to allow some apps to index the launcher (e.g. Kindle Fire)
+}
